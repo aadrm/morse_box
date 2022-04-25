1d943911f1bc8e9d4e31918920c92d7fd1579257
Antonio Adrian <helloworld@antonioadrian.dev>
2022-04-25 15:28:25 +0000

    Update wanted_morse.ino
    added comments

--- arduino/wanted_morse/wanted_morse.ino
+++ arduino/wanted_morse/wanted_morse.ino
@@ -9,10 +9,8 @@ const int duty = 60; // modulates the sound produced by the buzzer
 const int tempo = 165; // base unit of time
 const int dit = tempo; // morse dot or time between dots and dashes
 const int dah = tempo * 3; // morse dash, it's standard to be 3 times longer than the dot
-const int duh = dit * 5; // used as time between characters
-const int wordDelay = tempo * 15; // 7 times longer than a dot is the standard separation between words
-
-
+const int duh = dit * 5; // used as time between characters, it's not a standard value but it's used to make the game a bit easier.
+const int wordDelay = tempo * 12; // 7 times longer than a dot is the standard separation between words
 
 void setup() {
     pinMode(buzzer, OUTPUT);
@@ -23,31 +21,41 @@ void loop() {
     playMorseChar("---");   // O
     playMorseChar(".-..");  // L
     playMorseChar("-");     // T
-    delay(wordDelay - dah);
+    delay(wordDelay);
 }
 
 void playMorseChar(String letter) {
     for(int i = 0; i < letter.length(); i++) {
+        /*
+        Goes through the characters of a morse word (a morse word being a single alphabet character) and plays it.
+        */
         char c = letter[i];
-
-        // start playing signal
+        // start signal
         analogWrite(buzzer, duty);
-        // signal duration depending if its a dot or a dash
-        if(c == '.') {
-            delay(dit);
-        }
-        else {
-            delay(dah);
-        }
-
+        // signal duration
+        delay(charDuration(c));
         // stop the signal
         noTone(buzzer);
-        // if not the last signal of the character, make a short stop otherwise a longer stop
-        if (i < letter.length() - 1){
+        // if not the last dit or dat of the character make a short stop otherwise a longer stop
+        if (i < letter.length() - 1) {
             delay(dit);
         }
         else {
             delay(duh);
         }
     }
+}
+
+int charDuration(char c) {
+    /*
+    Returns the duration of the character, ignores anything that's not a period or a hyphen.
+    */
+    int duration = 0;
+    if (c == '.') {
+        duration = dit;
+    }
+    else if (c == '-') {
+        duration = dah;
+    }
+    return duration
 }
